65d9a9f1846b14a47832b40bd357c69e4ab943e5
Kevin Yu <kuanweiyu@gmail.com>
2020-01-14 05:06:43 -0500

    update password

--- password.py
+++ password.py
@@ -1,15 +1,13 @@
 correctPass = 'a123456'
 count = 3
 
-while True:
+while count > 0:
 	password = input('Please enter your password: ')
 	count = count - 1 
 	if password == correctPass:
 		print('Login successful!')
 		break
 	else:
-		if count == 0:
-			print('You try too many wrong times!')
-			break
-		else:
-			print('Incorrect password! You still have', count,'chances')
+		print('Incorrect password! You still have', count,'chances')
+		
+print('You try too many wrong times!')
