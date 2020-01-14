9b186f128d1f556469e06bc9302c69694cedccbd
Kevin Yu <kuanweiyu@gmail.com>
2020-01-14 04:21:15 -0500

    add driving

---
+++ driving.py
@@ -0,0 +1,14 @@
+country = input('Where are you from: ')
+age = input('What is your age: ')
+age = int(age)
+
+if country == 'Taiwan':
+	if age >= 18:
+		print('You can drive.')
+	else:
+		print('You can not drive.')
+elif country == 'USA':
+	if age >= 16:
+		print('You can drive.')
+	else:
+		print('You can not drive.')
