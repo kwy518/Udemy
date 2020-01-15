28a061554318ccb013129452a117c756021558bf
Kevin Yu <kuanweiyu@gmail.com>
2020-01-15 02:47:12 -0500

    update

--- product.py
+++ product.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('Please enter the price of the product: ')
+	price = int(price)
 	#p = []
 	#p.append(name)
 	#p.append(price)
@@ -12,4 +13,9 @@ while True:
 print(products)
 
 for p in products:
-	print(p)
+	print('The price of', p[0], 'is', p[1])
+
+with open('products.csv', 'w') as f:
+	f.write('Product, Price\n')  # add product and price two title
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
