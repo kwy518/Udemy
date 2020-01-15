cb2d88435c9143559fd15039e6b6693dec1a6c53
Kevin Yu <kuanweiyu@gmail.com>
2020-01-15 02:59:44 -0500

    update

--- product.py
+++ product.py
@@ -1,20 +1,32 @@
+# read a file
 products = []
+with open('products.csv', 'r') as f1:
+	for line in f1:
+		if 'Product, Price' in line:
+			continue
+		s = line.strip().split(',')
+		name = s[0]
+		price = s[1]
+		products.append([name, price])
+
+
+
+
 while True:
 	name = input('Plase enter a product: ')
 	if name == 'q':
 		break
 	price = input('Please enter the price of the product: ')
 	price = int(price)
-	#p = []
-	#p.append(name)
-	#p.append(price)
 	p = [name, price]
 	products.append(p)
 print(products)
 
+# Print all the purchase histories
 for p in products:
 	print('The price of', p[0], 'is', p[1])
 
+# Write a file
 with open('products.csv', 'w') as f:
 	f.write('Product, Price\n')  # add product and price two title
 	for p in products:
